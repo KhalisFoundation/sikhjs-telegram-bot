f829f00f90cb73906d5dd96b9696478d04b702a5
Sukhmanpreet Singh <33250386+SukhmanpreetSingh@users.noreply.github.com>
2018-07-04 14:51:36 +0530

    Fix #1 (#3)

--- index.js
+++ index.js
@@ -39,27 +39,27 @@ bot.onText(/\/help/, ({ chat: { id } }) =>
 ਵਾਹਿਗੁਰੂ ਜੀ ਕਾ ਖਾਲਸਾ
 ਵਾਹਿਗੁਰੂ ਜੀ ਕੀ ਫਤਹਿ
 
-Welcome to SikhJSBot
+Welcome to SikhiToTheMax GurbaniBot
 
 I can help you fetch Gurbani right from Telegram.
 
-* Use \`/search@SikhJSBot jqmvh\` to search a shabad that has a line containing first letters of words as
+* Use \`/search@GurbaniBot jqmvh\` to search a shabad that has a line containing first letters of words as
   ਜ(j) ਤ(q) ਮ(m) ਵ(v) ਹ(h)
   Use the following image to learn how to type Gurmukhi in Roman Script.
   * Search results of above query will have links like \`/s123\`. Click those to view the shabad.
 
-* Use \`/sggs123@SikhJSBot\` to load Ang 123 from Sri Guru Granth Sahib Jee.
+* Use \`/sggs123@GurbaniBot\` to load Ang 123 from Sri Guru Granth Sahib Jee.
 
-* Use \`/dg123@SikhJSBot\` to load Ang 123 from Dasveh Patsaahi Ka Granth (aka Dasam Granth, Bachitar Natak Granth)
+* Use \`/dg123@GurbaniBot\` to load Ang 123 from Dasveh Patsaahi Ka Granth (aka Dasam Granth, Bachitar Natak Granth)
 
-You can always user \`/help@SikhJSBot\` to bring this information again.
+You can always user \`/help@GurbaniBot\` to bring this information again.
 Contact @bogas04 in case of any issue.
   `
     )
     .then(() =>
       bot.sendPhoto(
         id,
-        "http://www.jattsite.com/infos/infosbilder/03a-anlmolLipi-key-map.jpg"
+        "https://www.sikhitothemax.org/assets/images/help/web-desktop-keyboard-map.png"
       )
     )
 );
