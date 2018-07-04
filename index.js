0c4a23675fe8d1519b42e3d890392d2596544453
Divjot Singh <bogas04@gmail.com>
2018-07-04 14:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ bot.onText(/\/help/, ({ chat: { id } }) => bot
 ਵਾਹਿਗੁਰੂ ਜੀ ਕਾ ਖਾਲਸਾ
 ਵਾਹਿਗੁਰੂ ਜੀ ਕੀ ਫਤਹਿ
 
-Welcome to sttmBot
+Welcome to SikhiToTheMax GurbaniBot
 
 I can help you fetch Gurbani right from Telegram.
 
-* Use \`/search@sttmBot jqmvh\` to search a shabad that has a line containing first letters of words as
+* Use \`/search@GurbaniBot jqmvh\` to search a shabad that has a line containing first letters of words as
   ਜ(j) ਤ(q) ਮ(m) ਵ(v) ਹ(h)
   Use the following image to learn how to type Gurmukhi in Roman Script.
   * Search results of above query will have links like \`/s123\`. Click those to view the shabad.
 
-* Use \`/sggs123@sttmBot\` to load Ang 123 from Sri Guru Granth Sahib Jee.
+* Use \`/sggs123@GurbaniBot\` to load Ang 123 from Sri Guru Granth Sahib Jee.
 
-* Use \`/dg123@sttmBot\` to load Ang 123 from Dasveh Patsaahi Ka Granth (aka Dasam Granth, Bachitar Natak Granth)
+* Use \`/dg123@GurbaniBot\` to load Ang 123 from Dasveh Patsaahi Ka Granth (aka Dasam Granth, Bachitar Natak Granth)
 
-You can always user \`/help@sttmBot\` to bring this information again.
+You can always user \`/help@GurbaniBot\` to bring this information again.
 Contact @bogas04 in case of any issue.
   `)
   .then(() => bot.sendPhoto(id, 'http://www.jattsite.com/infos/infosbilder/03a-anlmolLipi-key-map.jpg'))
